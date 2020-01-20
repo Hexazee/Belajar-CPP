de03736f659053252ec39a5fb7cc8675928741f0
Hexazee <danangpostman37@gmail.com>
2020-01-20 21:24:49 +0700

    menambahkan null pointer

--- pointer.cpp
+++ pointer.cpp
@@ -5,11 +5,13 @@ using namespace std;
 int main() {
 
     int myVar;
+    int *_Null = NULL;
     int *PointerMyVAR = &myVar; // Address
 
     cout << "Masukan nilai : "; cin >> myVar;
     cout << "\n  Nilai myVar : " << myVar << endl;
     cout << "Address myVar : " << PointerMyVAR << endl;
+    cout << "\n Pointer Null : " << _Null << endl;
 
     return 0;
 
